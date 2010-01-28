c1b6dc34373d0ff0f89902b553eb9a1e420447c0
Martin Gajdos <unabatede@gmail.com>
2010-01-28 21:49:39 +0100

    changed formatting inside README.md, extended webrat tests and also finally checked them in

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ Copyright (c) 2009, 2010 Martin Gajdos
 released under the MIT license
 
 Introduction
-============
+------------
 
 Shagit is a simple web application for managing your local git repositories. There's no need for a database and repositories are being privately published using ssh.
 
 Dependencies
-============
+------------
 
 * Sinatra
 * Haml
@@ -21,38 +21,38 @@ Dependencies
 * Webrat
 
 Installation
-============
+------------
 
 You will need a working installation of Ruby >=1.8.6, RubyGems >= 1.3.5 and Git >= 1.5
 
 Clone the repository:
 
-git clone git://github.com/unabatede/shagit.git
+    git clone git://github.com/unabatede/shagit.git
 
 Install the Sinatra, Haml & Grit gems:
 
-gem install sinatra haml grit rack-test webrat
+    gem install sinatra haml grit rack-test webrat
 
 Run it (you should consider other options for production deployment):
 
-cd shagit
-ruby shagit.rb -e production
+    cd shagit
+    ruby shagit.rb -e production
 
 Deployment
-============
+----------
 
 Using Apache & Phusion Passenger and the following to a new virtual host file (adjust to suite your needs):
 
-<VirtualHost *:80>
-    DocumentRoot /var/www/shagit/public
-    <Directory /var/www/shagit/public>
-        Allow from all
-        Options -MultiViews
-    </Directory>
-</VirtualHost>
+    <VirtualHost *:80>
+        DocumentRoot /var/www/shagit/public
+        <Directory /var/www/shagit/public>
+            Allow from all
+            Options -MultiViews
+        </Directory>
+    </VirtualHost>
 
 
 Also, inside Shagit's folder, create a 'tmp' and 'public' folder:
 
-cd /var/www/shagit
-mkdir tmp; mkdir public
+    cd /var/www/shagit
+    mkdir tmp; mkdir public
---
+++ test/acceptance/webrat-test.rb
@@ -0,0 +1,82 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "../../lib"))
+
+require '../../shagit_app'
+
+require "rack/test"
+require "webrat"
+require "test/unit"
+
+use Rack::Session::Cookie
+
+Webrat.configure do |config|
+  config.mode = :rack
+  config.application_port = 4567
+  config.application_framework = :sinatra
+end
+
+class AppTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+  include Webrat::Methods
+  include Webrat::Matchers
+
+  def app
+    Sinatra::Application.new
+  end
+
+  def test_00_log_in_screen_displayed
+    visit "/"
+    assert_contain("log in")
+    assert_contain("username")
+    assert_contain("password")
+    assert_contain("login")
+  end
+
+  def test_01_logging_in
+    visit "/login"
+    fill_in "username", :with => "admin"
+    fill_in "password", :with => "admin"
+    click_button "login"
+    assert_contain("all repositories")
+  end
+
+  def test_02_no_repositories_available
+    test_01_logging_in
+    visit "/"
+    assert_contain("None found?!")
+  end
+
+  def test_03_adding_new_repository
+    test_01_logging_in
+    visit "/"
+    click_link "create your first repository"
+    assert_contain("create new repository")
+    fill_in "name", :with => "webrat"
+    click_button "create"
+    assert_contain("Repository webrat created successfully")
+  end
+
+  def test_04_displaying_info_on_existing_repository
+    test_01_logging_in
+    visit "/"
+    click_link "webrat"
+    assert_contain("display repository")
+  end
+
+#  def test_05_deleting_repository
+#    test_01_logging_in
+#    visit "/"
+#    click_link "webrat"
+#    assert_contain("display repository")
+#    click_button "delete"
+#    save_and_open_page
+#    assert_contain("Are you sure you want to delete the following repository?")
+#    click_button "yes"
+#  end
+
+#  def test_06_logging_out
+#    test_01_logging_in
+#    visit "/"
+#    click_link "log out"
+#    assert_contain("you have successfully been logged out. see you next time! ")
+#  end
+end
